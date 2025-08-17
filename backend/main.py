b2c0fc79f25f267b380092578e719b3ddb98039a
codecarts <codecarthelp@gmail.com>
2025-08-17 23:13:22 +0530

    fix: add python-multipart dependency

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="codecart API")
 # Configure CORS to allow requests from your deployed frontends
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
